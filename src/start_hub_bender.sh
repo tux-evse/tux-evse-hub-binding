9733a4a950e64a70bbd94fda3f7a6abb2503d8c3
Ronan Le Martret <ronan.lemartret@iot.bzh>
2023-11-17 18:00:37 +0100

    init
    Signed-off-by: Ronan Le Martret <ronan.lemartret@iot.bzh>

--- src/start_hub_bender.sh
+++ src/start_hub_bender.sh
@@ -6,6 +6,6 @@ for API in $(ls /run/user/${UID}/apis/ws/); do
     LIST_API="${LIST_API} --ws-client unix:/run/user/${UID}/apis/ws/${API}"
 done
 
-/usr/bin/afb-binder -M -p 8080 -vvv \
+/usr/bin/afb-binder -M -p 8081 -vvv \
     ${LIST_API} \
     --roothttp /usr/redpesk/tux-evse-hub-binding/htdocs
