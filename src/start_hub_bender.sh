7469741797152fc311c1d2281cc5b3a618e97ccf
Ronan Le Martret <ronan.lemartret@iot.bzh>
2023-11-17 16:45:27 +0100

    init
    Signed-off-by: Ronan Le Martret <ronan.lemartret@iot.bzh>

--- src/start_hub_bender.sh
+++ src/start_hub_bender.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/bash
 
-/usr/bin/afb-binder -M -p 8080 -vvv --roothttp /usr/redpesk/tux-evse-hub-binding/htdocs
+/usr/bin/afb-binder -M -p 8080 -vvv \
+    --ws-client unix:/run/user/${UID}/apis/ws/helloworld \
+    --roothttp /usr/redpesk/tux-evse-hub-binding/htdocs
