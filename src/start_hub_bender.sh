784615f410e471ce0ac8f3a117d668dfe7613dc0
Ronan Le Martret <ronan.lemartret@iot.bzh>
2023-11-20 18:00:17 +0100

    update manifest
    Signed-off-by: Ronan Le Martret <ronan.lemartret@iot.bzh>

--- src/start_hub_bender.sh
+++ src/start_hub_bender.sh
@@ -3,7 +3,7 @@
 LIST_API=""
 
 for API in $(ls /run/user/${UID}/apis/ws/); do
-    LIST_API="${LIST_API} --ws-client unix:/run/user/${UID}/apis/ws/${API}"
+    LIST_API="${LIST_API} --ws-client public:unix:/run/user/${UID}/apis/ws/${API}"
 done
 
 /usr/bin/afb-binder -M -p 8081 -vvv \
