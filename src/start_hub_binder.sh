358492ecec6c5fb73d99dc27602a368b5d1f0f33
Sebastien Douheret <sebastien.douheret@iot.bzh>
2023-12-14 11:45:05 +0100

    Fix startup script and rename to binder
    Change-Id: I66a5ad6d1ed2dcad52e1487ddd5ba91c48e6e02e
    Signed-off-by: Sebastien Douheret <sebastien.douheret@iot.bzh>

--- src/start_hub_binder.sh
+++ src/start_hub_binder.sh
@@ -6,6 +6,6 @@ for API in $(ls /run/user/${UID}/apis/ws/); do
     LIST_API="${LIST_API} --ws-client public:unix:/run/user/${UID}/apis/ws/${API}"
 done
 
-/usr/bin/afb-binder -M -p 8081 -vvv \
+/usr/bin/afb-binder -M -p 1234 -vvv \
     ${LIST_API} \
     --roothttp /usr/redpesk/tux-evse-webapp/htdocs
