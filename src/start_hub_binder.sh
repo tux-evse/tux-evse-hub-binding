0cbb01e0b6ddf29962114ca48b7348dde240c830
jobol <jose.bollo@iot.bzh>
2023-12-19 15:35:48 +0100

    start_hub_binder.sh: Move to auto-api
    Instead of listing the API in /run/user/$UID/api/ws, use the
    option --auto-api pointing that directory

--- src/start_hub_binder.sh
+++ src/start_hub_binder.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/bash
 
-LIST_API=""
-
-for API in $(ls /run/user/${UID}/apis/ws/); do
-    LIST_API="${LIST_API} --ws-client public:unix:/run/user/${UID}/apis/ws/${API}"
-done
-
 /usr/bin/afb-binder -M -p 1234 -vvv \
-    ${LIST_API} \
-    --roothttp /usr/redpesk/tux-evse-webapp/htdocs
+    --auto-api=/run/user/${UID}/apis/ws \
+    --roothttp=/usr/redpesk/tux-evse-webapp/htdocs
